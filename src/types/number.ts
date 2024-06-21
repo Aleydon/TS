8eaf9d60233f33a36130735b507e53f896762944
Aleydon <aleydonroberto@gmail.com>
2024-06-21 00:58:57 -0300

    ADD: Number type

---
+++ src/types/number.ts
@@ -0,0 +1,10 @@
+// number accepts (int, float, hex, binary)
+
+const year = 2024;
+const age = 29;
+const height: number = 1.7;
+
+console.log(`Year: ${year}, Age: ${age}, Height: ${height}`);
+console.log(`Number sum: ${year + age + height}`);
+
+console.log(30 + '50'); // numbers + strings are concatenated
