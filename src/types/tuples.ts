3065366e089101723a748e48f81638c29dea814f
Aleydon <aleydonroberto@gmail.com>
2024-11-03 17:02:07 -0300

    ADD: Tuples

---
+++ src/types/tuples.ts
@@ -0,0 +1,9 @@
+//  The tuple does not exist in javascript, only in typescript
+//  Tuples must follow the standards for the type and number of values passed
+//  Example: 2 values, the first number and the second string
+
+const student: [number, string] = [2024, 'Aleydon'];
+const orderStatus: [number, string] = [200, 'OK'];
+
+console.log(student);
+console.log(orderStatus);
