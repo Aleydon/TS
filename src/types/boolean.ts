7c31a0f80ff5253783bbcf3155e39db027ecdba1
Aleydon <aleydonroberto@gmail.com>
2024-11-03 15:49:41 -0300

    ADD: Boolean type

---
+++ src/types/boolean.ts
@@ -0,0 +1,7 @@
+// boolean accepts (true, false)
+const isAuthenticated: boolean = true;
+
+console.log(isAuthenticated ? 'Authenticated' : 'Not authenticated');
+
+const isAuthenticated2 = Boolean(0);
+console.log(isAuthenticated2 ? 'Authenticated' : 'Not authenticated');
