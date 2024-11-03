101670a617e7d9b5ac3fe1c97fb3cca319ebba46
Aleydon <aleydonroberto@gmail.com>
2024-11-03 17:33:48 -0300

    ADD: Type Null

---
+++ src/types/null.ts
@@ -0,0 +1,10 @@
+// Null is a variable that contains no value.
+
+const myUser: null = null;
+const MyName: string | null = 'Aleydon';
+
+console.log(myUser);
+console.log(typeof myUser);
+
+console.log(MyName);
+console.log(typeof MyName);
