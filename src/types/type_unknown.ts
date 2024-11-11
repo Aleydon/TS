0f864bbcf760cc6afad3efeb5d8e3c14bdf29174
Aleydon <aleydonroberto@gmail.com>
2024-11-11 12:38:48 -0300

    ADD: Type Unknown

---
+++ src/types/type_unknown.ts
@@ -0,0 +1,13 @@
+// For example when you don't know the type of API
+
+let total: unknown;
+
+console.log(total);
+console.log(typeof total);
+
+// Value of type Unknown cannot be assigned to type Unknown or Any
+
+// let total2: unknown;
+// const total3: number = total2;
+// console.log(total3);
+// console.log(typeof total3);
